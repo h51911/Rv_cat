3ff97df9887c96ea594b088c26b57462eb3d8b2a
zhengfuzlfll <3250856211@qq.com>
2020-01-06 16:09:22 +0800

    1.0.2

--- react-project/src/pages/Choose.jsx
+++ react-project/src/pages/Choose.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import '../style/choose.scss'
+import React from "react";
+import "../style/choose.scss";
+
+import Header from '../components/Header';
+
+/* antd */
+
+
 class Choose extends React.Component {
-    render() {
-        return (
-            <div className="choose">Choose
-            <p>fdsafdsafdsa</p></div>
-        );
-    }
+  render() {
+    return (
+      <div className="choose">
+        <Header></Header>
+      </div>
+    );
+  }
 }
 export default Choose;

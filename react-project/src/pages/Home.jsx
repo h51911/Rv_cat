46703c372a250dbf9a8e8248318ec82d2a9c9fa7
yuanchao1234 <156283662@qq.com>
2020-01-06 15:29:24 +0800

    0106-15:27

--- react-project/src/pages/Home.jsx
+++ react-project/src/pages/Home.jsx
@@ -3,7 +3,10 @@ import "../style/Home.scss";
 class Home extends React.Component {
     render() {
         return (
-            <div className="home">Home</div>
+            <div className="home">
+                <div className="header">fdsa</div>
+                <p>Home</p>
+            </div>
         );
     }
 }

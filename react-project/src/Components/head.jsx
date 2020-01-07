9ff06bd03a31226ff516e75fef9177c4c6b3f341
luoxiaoluo97 <56592301+luoxiaoluo97@users.noreply.github.com>
2020-01-07 09:48:50 +0800

    luozhijie
    123457

--- react-project/src/Components/head.jsx
+++ react-project/src/Components/head.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import '../style/Head.scss'
 
 //引入字体图标
-import '../utils/iconfont/iconfont.css'
+import '../assets/iconfont/iconfont.css'
 
 function Head() {
     return (
         // <div classNameName="box">头部</div>
         <div className="nav">
             <p className="p_1">
-                <span className="span_1">&lt;</span>
+                <span className="span_1 iconfont icon-jiantou1"></span>
                 <span className="span_2">返回</span>
             </p>
             <div className="p_2">
                 <span>二手房车</span>
-                <i ></i>
+                <i className=" i_1 iconfont icon-jiantou"></i>
             </div>
             <p className="p_3">首页</p>
         </div>

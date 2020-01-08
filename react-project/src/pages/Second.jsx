811344708a1dabe5fc841c6361ce809b02edbe15
luoxiaoluo97 <56592301+luoxiaoluo97@users.noreply.github.com>
2020-01-08 10:03:51 +0800

    骆志杰 1.08

--- react-project/src/pages/Second.jsx
+++ react-project/src/pages/Second.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../style/Second.scss'; // 引入外部样式
-import Head from '../Components/head.jsx'
+// import Head from '../Components/head.jsx'
+import Head from '../components/head.jsx'
 
 class Second extends Component {
 

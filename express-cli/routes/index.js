51f26044e4c66fa0ff5d1b271a6bcc0d39f52116
yuanchao1234 <156283662@qq.com>
2020-01-08 11:50:22 +0800

    0108-11:50

--- express-cli/routes/index.js
+++ express-cli/routes/index.js
@@ -11,6 +11,8 @@ router.get('/', function (req, res, next) {
 router.get('/home', async (req, res, next) => {
   res.append('Access-Control-Allow-Origin', '*');
   let data = await find('home');
+  console.log(2);
+
   res.json(data);
 });
 module.exports = router;

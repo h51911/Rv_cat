6f913290babf5436df796fe31d6a1ea495cb31b0
luoxiaoluo97 <56592301+luoxiaoluo97@users.noreply.github.com>
2020-01-09 19:55:25 +0800

    二手车页面

--- react-project/src/pages/Second.jsx
+++ react-project/src/pages/Second.jsx
@@ -112,7 +112,32 @@ class Second extends Component {
                     <i className="iconfont icon-quxiaozhiding"></i>
                     <span>房车之家二手房车板块管理条例</span>
                 </div>
+                <div className="oneList">
+                    <li className="list-item">
 
+                        <div className="car-img">
+                            <img alt="上海二手房车2017年上牌大通原厂C型房车店里现车多" style={{
+                                width: "500",
+                                display: 'block'
+                            }} src="https://www.rv28.com/data/attachment/forum/threadcover/78/f7/21299.jpg" />
+
+                        </div>
+                        <div className="car-list">
+                            <h3 className="car-name">[<em>出售</em>] 上海二手房车2017年上牌大通原厂C型房车店里现车多</h3>
+                            <div className="ar-km">
+                                <span className="z">3万公里 / 2017-08-12 / 上海</span>
+                                <div className="car-title">
+                                    <span className="y" id="21299">￥<em>23</em>万</span>
+                                    <span className="threadAttr" style={{
+                                        color: '#ff4f53', border: '1px solid #ff4f53'
+                                    }}>置顶</span>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </li>
+                </div>
 
                 <Switch>
                     <Route path="/second/all" component={All} ></Route>
--- react-project/src/style/second/All.jsx
+++ react-project/src/style/second/All.jsx
@@ -6,7 +6,28 @@ class All extends Component {
     }
     render() {
         return (
-            <div>all</div>
+            <div>
+                {/* <li className="list-item cl">
+                    <a href="https://used.rv28.com/thread-21299-1.html" className="car-info">
+                        <div className="car-img">
+                            <img alt="上海二手房车2017年上牌大通原厂C型房车店里现车多" style={{
+                                width: "500"
+                            }} src="https://www.rv28.com/data/attachment/forum/threadcover/78/f7/21299.jpg" style={{
+                                display: 'block'
+                            }} />
+
+                        </div>
+                        <h3 className="car-name">[<em>出售</em>] 上海二手房车2017年上牌大通原厂C型房车店里现车多</h3>
+                        <div className="car-km">
+                            <span className="z">3万公里 / 2017-08-12 / 上海</span>
+                            <span className="y" id="21299">￥<em>23</em>万</span>
+                            <span className="threadAttr" style=" color:#ff4f53; border:1px solid #ff4f53;">置顶</span>
+                        </div>
+
+                    </a>
+                </li> */}
+                123
+            </div>
         )
     }
 }

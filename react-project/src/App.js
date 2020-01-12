62ba29668b97ae65ffc4d89eb7aebc8695e006e9
yuanchao1234 <156283662@qq.com>
2020-01-12 16:22:40 +0800

    优化1.0

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Link, NavLink, Switch, Redirect, withRouter } from 'react-router-dom';
 import './App.scss';
-import Home from './pages/Home.jsx';
-import Choose from './pages/Choose.jsx';
-import Second from './pages/Second.jsx';
-import Mine from './pages/Mine.jsx';
-import Video from './pages/Video.jsx';
-import VideoXiang from './pages/VideoXiang.jsx';
-import Reg2 from './pages/Reg2.jsx';
-import Login2 from './pages/Login2.jsx';
-import Space from './pages/Space.jsx';
-import Search from './pages/Search';
+
+// import Home from './pages/Home.jsx';
+// import Choose from './pages/Choose.jsx';
+// import Second from './pages/Second.jsx';
+// import Mine from './pages/Mine.jsx';
+// import Video from './pages/Video.jsx';
+// import VideoXiang from './pages/VideoXiang.jsx';
+// import Reg2 from './pages/Reg2.jsx';
+// import Login2 from './pages/Login2.jsx';
+// import Space from './pages/Space.jsx';
+// import DetailPage from "./components/DetailPage";
+// import List from "./components/List";
+// import Search from './pages/Search';
 
 import './style/font_hr5tn4apse8/iconfont.css';
 import { TabBar } from 'antd-mobile';
-import DetailPage from "./components/DetailPage";
-import List from "./components/List";
+
+//路由按需加载优化
+const Home = lazy(() => import("./pages/Home"));
+const Choose = lazy(() => import("./pages/Choose.jsx"));
+const Second = lazy(() => import("./pages/Second.jsx"));
+const Mine = lazy(() => import("./pages/Mine.jsx"));
+const Video = lazy(() => import("./pages/Video.jsx"));
+const VideoXiang = lazy(() => import("./pages/VideoXiang.jsx"));
+const Reg2 = lazy(() => import("./pages/Reg2.jsx"));
+const Login2 = lazy(() => import("./pages/Login2.jsx"));
+const Space = lazy(() => import("./pages/Space.jsx"));
+const DetailPage = lazy(() => import("./components/DetailPage"));
+const List = lazy(() => import("./components/List"));
+const Search = lazy(() => import("./pages/Search"));
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -23,26 +39,28 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Switch>
-          <Route path='/home' component={Home} />
-          <Route path='/choose' component={Choose} />
-          <Route path='/second' component={Second} />
-          <Route path='/mine' component={Mine} />
-          <Route path='/video2' component={Video} exact />
-          <Route path="/detailpage" component={DetailPage} />
-          <Route path="/list" component={List} />
-          <Route path='/reg2' component={Reg2} />
-          <Route path='/login2' component={Login2} />
-          <Route path='/space' component={Space} />
-          <Route path='/search' component={Search} />
-          {/*嵌套路由*/}
-          <Route path='/video2/xiang/:id' component={VideoXiang} />
-          <Route path='/notfound' render={() => <h1>你访问的页面不存在</h1>} />
-          {/* "/" 跳转到 "/home" */}
-          <Redirect from="/" to="/home" exact />
-          {/* 404 */}
-          <Redirect to="/notfound" />
-        </Switch>
+        <Suspense fallback={<div>加载中...</div>}>
+          <Switch>
+            <Route path='/home' component={Home} />
+            <Route path='/choose' component={Choose} />
+            <Route path='/second' component={Second} />
+            <Route path='/mine' component={Mine} />
+            <Route path='/video2' component={Video} exact />
+            <Route path="/detailpage" component={DetailPage} />
+            <Route path="/list" component={List} />
+            <Route path='/reg2' component={Reg2} />
+            <Route path='/login2' component={Login2} />
+            <Route path='/space' component={Space} />
+            <Route path='/search' component={Search} />
+            {/*嵌套路由*/}
+            <Route path='/video2/xiang/:id' component={VideoXiang} />
+            <Route path='/notfound' render={() => <h1>你访问的页面不存在</h1>} />
+            {/* "/" 跳转到 "/home" */}
+            <Redirect from="/" to="/home" exact />
+            {/* 404 */}
+            <Redirect to="/notfound" />
+          </Switch>
+        </Suspense>
         <div className="tab">
           <ul>
             <li>
